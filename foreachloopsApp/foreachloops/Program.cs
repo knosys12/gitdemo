2c6f49a5d5800f465c68e47878fa49b9a05b083a
arrenzo corbett <acorbett802@gmail.com>
2025-01-14 06:51:50 -0500

    test

---
+++ foreachloopsApp/foreachloops/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Enter comma-seperated first names");
+string data = Console.ReadLine();
+
+
+//string data = "tim,sue,bob,jane";
+List<string> firstNames = data.Split(',').ToList();
+//foreach (string name in firstNames)
+
+//for (int i = 0; i < firstNames.Count; i++)
+//{
+//    Console.WriteLine(firstNames[i]);
+//}
+
+foreach (string firstname in firstNames)
+{
+    Console.WriteLine(firstname);
+}
